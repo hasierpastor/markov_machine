d7bc5cd561253d0e02bba9b2d5ec16806d5180c6
Hasier Pastor <hasier.pastor@gmail.com>
2018-10-16 16:55:03 -0700

    add strip html feature

--- makeText.js
+++ makeText.js
@@ -1,2 +1,29 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markovFile = require('./markov');
+const axios = require('axios');
+let markovClass = markovFile.MarkovMachine;
+const stripHtml = require('string-strip-html');
 
+async function produceMarkov(path) {
+  if (process.argv[2] == 'file') {
+    let text = fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let mm = new markovClass(data);
+        console.log(mm.makeText());
+      }
+    });
+  } else if (process.argv[2] == 'url') {
+    try {
+      let resp = await axios.get(path);
+      let mm = new markovClass(stripHtml(resp.data));
+      console.log(mm.makeText());
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+produceMarkov(process.argv[3]);
--- markov.js
+++ markov.js
@@ -1,14 +1,12 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.words = words.filter(c => c !== '');
+    this.wordsObj = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,13 +15,63 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let wordsObj = {};
+    for (let i = 0; i < this.words.length; i++) {
+      if (this.words[i] in wordsObj) {
+        wordsObj[this.words[i]].push(this.words[i + 1]);
+      } else {
+        wordsObj[this.words[i]] = [this.words[i + 1]];
+      }
+    }
+    return wordsObj;
   }
-
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
+  makeText(numWords = 1000) {
     // TODO
+    let story = '';
+    let wordsObjLength = Object.keys(this.wordsObj).length;
+
+    //starting word, becomes random
+    let randomWord;
+    for (let key in this.wordsObj) {
+      if (firstCapital(this.wordsObj[key]) !== undefined) {
+        randomWord = firstCapital(this.wordsObj[key]);
+        break;
+      }
+    }
+    console.log(randomWord);
+    for (let i = 0; i < numWords; i++) {
+      let randNum = Math.floor(
+        Math.random() * this.wordsObj[randomWord].length
+      );
+
+      randomWord = this.wordsObj[randomWord][randNum];
+      if (randomWord === undefined) {
+        return story;
+      }
+
+      if (
+        randomWord.substring(randomWord.length - 1) === '.' &&
+        i > Math.floor(numWords * 0.75)
+      ) {
+        return (story += randomWord);
+      }
+      story += randomWord + ' ';
+    }
+    return story;
+  }
+}
+
+module.exports = {
+  MarkovMachine
+};
+
+function firstCapital(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== undefined && arr[i][0].toUpperCase() === arr[i][0]) {
+      return arr[i];
+    }
   }
 }
